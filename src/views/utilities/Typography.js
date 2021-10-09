0b487d956ef817361f3fd830764db1f88def3440
FrancisGanio <92006134+FrancisGanio@users.noreply.github.com>
2021-10-09 19:18:23 +0800

    updated

--- src/views/utilities/Typography.js
+++ src/views/utilities/Typography.js
@@ -15,42 +15,6 @@ const Typography = () => (
     <MainCard title="Products" secondary={<SecondaryAction link="https://next.material-ui.com/system/typography/" />}>
         <Grid container spacing={gridSpacing}>
             <Grid item xs={12} sm={6}>
-                <SubCard title="Heading">
-                    <Grid container direction="column" spacing={1}>
-                        <Grid item>
-                            <MuiTypography variant="h1" gutterBottom>
-                                h1. Heading
-                            </MuiTypography>
-                        </Grid>
-                        <Grid item>
-                            <MuiTypography variant="h2" gutterBottom>
-                                h2. Heading
-                            </MuiTypography>
-                        </Grid>
-                        <Grid item>
-                            <MuiTypography variant="h3" gutterBottom>
-                                h3. Heading
-                            </MuiTypography>
-                        </Grid>
-                        <Grid item>
-                            <MuiTypography variant="h4" gutterBottom>
-                                h4. Heading
-                            </MuiTypography>
-                        </Grid>
-                        <Grid item>
-                            <MuiTypography variant="h5" gutterBottom>
-                                h5. Heading
-                            </MuiTypography>
-                        </Grid>
-                        <Grid item>
-                            <MuiTypography variant="h6" gutterBottom>
-                                h6. Heading
-                            </MuiTypography>
-                        </Grid>
-                    </Grid>
-                </SubCard>
-            </Grid>
-            <Grid item xs={12} sm={6}>
                 <SubCard title="Sub title">
                     <Grid container direction="column" spacing={1}>
                         <Grid item>
