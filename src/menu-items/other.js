02eea95f6a8bb0ea3dfd9fd005f4d91f2b204e71
JoshuaSntgo <91967969+JoshuaSntgo@users.noreply.github.com>
2021-10-09 22:59:01 +0800

    up

--- src/menu-items/other.js
+++ src/menu-items/other.js
@@ -13,9 +13,25 @@ const icons = {
 const other = {
     id: 'sample-docs-roadmap',
     type: 'group',
-    children: 
-    
-{}
+    children: [
+        {
+            id: 'sample-page',
+            title: 'Sample Page',
+            type: 'item',
+            url: '/sample-page',
+            icon: icons.IconBrandChrome,
+            breadcrumbs: false
+        },
+        {
+            id: 'documentation',
+            title: 'Documentation',
+            type: 'item',
+            url: 'https://codedthemes.gitbook.io/berry/',
+            icon: icons.IconHelp,
+            external: true,
+            target: true
+        }
+    ]
 };
 
 export default other;
